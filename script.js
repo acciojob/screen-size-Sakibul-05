a405e9626dd30a936efe4138c4549a818a0dc622
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-05-30 21:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const width = document.querySelector("h1>span:nth-child(1)");
+        const height = document.querySelector("h1>span:nth-child(2)");
+
+        window.addEventListener("resize", ()=>{
+            width.innerText = window.innerWidth;
+            height.innerText = window.innerHeight;
+        })
